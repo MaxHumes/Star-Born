c21319edd2095b9878f57b335a858cf9c699cd6f
MaxHumes <maxhumes@live.com>
2020-09-16 12:25:36 -0400

    URP Update
    Updated project to use the Universal Render Pipeline so we can use post processing and shader graph

---
+++ Quick Platformer/Assets/Scripts/Camera Scripts/CameraPostProcessing.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraPostProcessing : MonoBehaviour
+{
+    void LateUpdate()
+    {
+        //currently unused script to be used for for changing post processing effects in focus mode
+        /*
+        if(Input.GetKey(KeyCode.F))
+        {
+            Camera.main.cullingMask = 1 << LayerMask.NameToLayer("Focus");
+        }
+        else
+        {
+            Camera.main.cullingMask = 1 << LayerMask.NameToLayer("Default");
+        }
+        */
+    }
+}
